5ced33696831b7cec0a687c5ba406c69ce132c98
Namgyu Yoo <namgyuyoo@Namgyuui-MacBookPro.local>
2021-02-25 15:49:09 +0900

    first commit

---
+++ flutter_app/lib/simple_app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      title: 'Hello flutter',
+      home: Scaffold(
+        appBar: AppBar(title: Text('Hello flutter')),
+        body: Text('Hello flutter'),
+      ),
+    ));
